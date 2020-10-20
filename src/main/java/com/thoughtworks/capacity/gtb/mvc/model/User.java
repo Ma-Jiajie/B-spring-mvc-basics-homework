19031c15b41bb8de92d452eec836d3da34fd44b4
jiajie <jiajie.ma@thoughtworks.com>
2020-10-20 10:57:34 +0800

    all requirements is done.

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/model/User.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/model/User.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import javax.validation.constraints.*;
 
 @Data
-@AllArgsConstructor
 public class User {
     @NotNull(message = "用户名不为空")
     @Pattern(regexp = "^[a-zA-Z0-9_]{3,10}$",message = "用户名不合法")
@@ -19,4 +18,34 @@ public class User {
 
     @Email(message = "邮箱地址不合法")
     private String email;
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
